ab4cdcbb0e60c4705d31d3e5e1112dc31217a2c1
NGuljajev <nguljajev@gmail.com>
2025-09-02 11:13:23 +0300

    Add types examples

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+
+echo "Hello, World!\n";
+echo ("Hello, World!\n");
+
+$var = 1;
+echo $var;
+echo 'Var is $var';
+echo "Var is $var\n";
+echo $var . "\n";
+$num = 1;
+$num = $num + 1;
+$num++;
+$num--;
+$num += 1;
+$num -= 1;
+$num *= 2;
+$num /= 3;
+$num = 5;
+$num %= 4;
+$num = 1.3;
+$num = 3;
+$num **= 2;
+$num = true;
+$num = false;
+$bar;
+$bar = null;
+var_dump($bar);
