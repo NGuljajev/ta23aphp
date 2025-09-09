00b2d911b435144bb63dd8d261d5504259be557b
NGuljajev <nguljajev@gmail.com>
2025-09-09 10:20:14 +0300

    boolean

--- index.php
+++ index.php
@@ -1,30 +1,7 @@
 <?php
 
-$array = [1, 2, 3];
-var_dump($array);
-$array = array(1, 2, 3);
-var_dump($array);
-$array = [1, 'hello', 3.5, true, null, [5, 6]];
-var_dump($array);
-
-
-$array = [
-    'name' => 'John',
-    'age' => 30,
-    'gender' => 'penis'
-
-];
-var_dump($array);
-
-
-
-$array = [
-    1,
-    2,
-    3,
-    'name' => 'John',
-    69,
-    'age' => 30,
-    'gender' => 'penis'
-];
-var_dump($array);
+$bool = true && false;
+$bool = true || false;
+$bool = !false;
+$bool = (true && false || true) && !(true || false && true); //false
+var_dump($bool);
