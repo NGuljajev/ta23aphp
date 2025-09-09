682a27259636288ae914d5869103484122feb8e5
NGuljajev <nguljajev@gmail.com>
2025-09-09 10:30:14 +0300

    add if and switch examples

--- index.php
+++ index.php
@@ -1,7 +1,38 @@
 <?php
 
-$bool = true && false;
-$bool = true || false;
-$bool = !false;
-$bool = (true && false || true) && !(true || false && true); //false
-var_dump($bool);
+$num = 15;
+if($num > 10){
+    var_dump("Number is greater than 10");  
+} else if ($num > 10){
+
+} else {
+
+}
+
+$day = date('w', strtotime('2023-06-15'));
+var_dump($day);
+switch($day){
+    case 0:
+        var_dump("It's Sunday");
+        break;
+    case 1:
+        var_dump("It's Monday");
+        break;
+    case 2:
+        var_dump("It's Tuesday");
+        break;
+    case 3:
+        var_dump("It's Wednesday");
+        break;
+    case 4:
+        var_dump("It's Thursday");
+        break;
+    case 5:
+        var_dump("It's Friday");
+        break;
+    case 6:
+        var_dump("It's Saturday");
+        break;
+    default:
+        var_dump("Invalid day");
+}
