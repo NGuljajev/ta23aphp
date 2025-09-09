e62bf17318be146fdad25e42e9a35f025df2ce29
NGuljajev <nguljajev@gmail.com>
2025-09-09 13:11:57 +0300

    add static example visibility and classes

--- index.php
+++ index.php
@@ -1,18 +1,60 @@
 <?php
+ 
+class Box {
+    public $width;
+    protected $height;
+    private $length;
+ 
+ 
+    public static $count = 0;
 
-
-for($i=0;$i<10;$i++){
-    if($i=3){
-        continue;
+    public static function test()
+    {
+        //var_dump($this->width);    
     }
-    if(i==5){
-        break;
+ 
+    public function __construct($w, $h, $l) 
+    {
+        self::$count++;
+        $this->width = $w;
+        $this->height = $h;
+        $this->length = $l;
     }
-    var_dump($i);
+ 
+    public function getWidth() {
+        return $this->width;
+    }
+    public function getWidth($width) 
+    {
+        if ($width > 0 && is_numeric($width)) {
+            $this->width = $width;
+        }
+    }
+ 
+    public function volume() {
+        return $this->width * $this->height * $this->length;
+    }
 }
+ 
+class MetalBox extends Box {
+    public $material = "Metal";
+    public $massPerUnit = 2;
+ 
+    public function changeWidth() {
+        $this->width = 333;
+    }
+ 
+    public function mass() {
+        return $this->volume() * $this ->massPerUnit;
+    }
+}
+ 
+$metalBox = new Box(2, 3, 4,);
+$metalBox::$count = 1;
+$box2 = new Box(2, 4, 5); 
+$box2 :: $count = 2; 
+var_dump($metalBox::$count, $box2::$count);
 
-for($i=0;$i<10;$i++){
-   for($j=0;$j<10;$j++){
-       break 2;
-   }
-}
+ 
+var_dump($metalBox);
+var_dump($metalBox->volume());
