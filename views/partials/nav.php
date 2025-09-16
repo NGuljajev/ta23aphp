4adce6f9d86dc4c889e0983386c4180b9411f740
NGuljajev <nguljajev@gmail.com>
2025-09-16 11:40:19 +0300

    add simple routers scripts

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -68,7 +68,7 @@
           <a class="nav-item nav-link link-body-emphasis active" href="index.php"
             >World</a
           >
-          <a class="nav-item nav-link link-body-emphasis" href="/us.php">U.S.</a>
+          <a class="nav-item nav-link link-body-emphasis" href="/us">U.S.</a>
           <a class="nav-item nav-link link-body-emphasis" href="#"
             >Technology</a
           >
