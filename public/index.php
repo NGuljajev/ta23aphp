fb3cc94518c10d38b3aa242dde8cf68fadfe567a
NGuljajev <nguljajev@gmail.com>
2025-09-23 13:14:01 +0300

    moved actions

--- public/index.php
+++ public/index.php
@@ -2,87 +2,28 @@
 if (preg_match('/\.(?:png|jpg|jpeg|gif|css|js)$/', $_SERVER["REQUEST_URI"])) {
     return false;    // serve the requested resource as-is.
 }
- 
-function dump(...$args){
-    echo'<pre>';
-    var_dump(...$args);
-    echo'</pre>';
-}
- 
-spl_autoload_register(function ($class){
+
+
+
+spl_autoload_register(function ($class) {
     $class = substr($class, 4);
-    require_once __DIR__ ."/../src/$class.php";
-});
-use App\Router;
-
-Router::addRoute('/', function () {
-    $posts = [
-        [
-            'title' => 'World news 1',
-            'published' => '16.09.2025',
-            'author' => 'Nikita',
-            'body' => 'Some world news body 1'
-        ],
-        [
-            'title' => 'World news 2',
-            'published' => '17.09.2025',
-            'author' => 'Nikita1',
-            'body' => 'Some world news body 2'
-        ],
-        [
-            'title' => 'World news 3',
-            'published' => '18.09.2025',
-            'author' => 'Nikita2',
-            'body' => 'Some world news body 3'
-        ],
-        [
-            'title' => 'World news 4',
-            'published' => '19.09.2025',
-            'author' => 'Nikita3',
-            'body' => 'Some world news body 4'
-        ],
-        [
-            'title' => 'World news 5',
-            'published' => '20.09.2025',
-            'author' => 'Nikita4',
-            'body' => 'Some world news body 5'
-        ],
-    ];
-    include __DIR__ . '/../views/index.php';
+    require_once __DIR__ . "/../src/$class.php";
 });
 
+require __DIR__ . '/../routes.php';
+require __DIR__ . '/../helpers.php';
 
-Router::addRoute('/us', function () {
-    $posts = [
-        [
-            'title' => ' U.S news 1',
-            'published' => '16.09.2025',
-            'author' => 'Ivan',
-            'body' => 'some U.S news body 1'
-        ],
-        [
-            'title' => 'U.S news 2',
-            'published' => '16.09.2024',
-            'author' => 'simon',
-            'body' => 'some U.S news body 2'
-        ],
-        [
-            'title' => 'U.S news 3',
-            'published' => '16.09.2023',
-            'author' => 'anatoly',
-            'body' => 'some U.S news body 3'
-        ]
-    ];
-    include __DIR__ . '/../views/us.php';
-});
 $router = new App\Router($_SERVER['REQUEST_URI']);
 $match = $router->match();
-if($match){
-    call_user_func($match['action']);
-}else{
-    echo '<img scr="https://http.cat/404>"';
+if ($match) {
+    if (is_callable($match['action'])) {
+        call_user_func($match['action']);
+    } else if (is_array($match['action'])) {
+        $class = $match['action'][0];
+        $controller = new $class();
+        $method = $match['action'][1];
+        $controller->$method();
+    } else {
+        echo '<img scr="https://http.cat/404>"';
+    }
 }
- 
- 
- 
- 
--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -1,8 +1,68 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controllers;
 
 class PublicController
 {
-    
+    public function index()
+    {
+        $posts = [
+            [
+                'title' => 'World news 1',
+                'published' => '16.09.2025',
+                'author' => 'Nikita',
+                'body' => 'Some world news body 1'
+            ],
+            [
+                'title' => 'World news 2',
+                'published' => '17.09.2025',
+                'author' => 'Nikita1',
+                'body' => 'Some world news body 2'
+            ],
+            [
+                'title' => 'World news 3',
+                'published' => '18.09.2025',
+                'author' => 'Nikita2',
+                'body' => 'Some world news body 3'
+            ],
+            [
+                'title' => 'World news 4',
+                'published' => '19.09.2025',
+                'author' => 'Nikita3',
+                'body' => 'Some world news body 4'
+            ],
+            [
+                'title' => 'World news 5',
+                'published' => '20.09.2025',
+                'author' => 'Nikita4',
+                'body' => 'Some world news body 5'
+            ],
+        ];
+        view('index', compact('posts'));
+    }
+
+    public function us() {
+    $posts = [
+        [
+            'title' => ' U.S news 1',
+            'published' => '16.09.2025',
+            'author' => 'Ivan',
+            'body' => 'some U.S news body 1'
+        ],
+        [
+            'title' => 'U.S news 2',
+            'published' => '16.09.2024',
+            'author' => 'simon',
+            'body' => 'some U.S news body 2'
+        ],
+        [
+            'title' => 'U.S news 3',
+            'published' => '16.09.2023',
+            'author' => 'anatoly',
+            'body' => 'some U.S news body 3'
+        ]
+    ];
+    view('us',  compact('posts'));
+}
+
 }
