cc2e7ce2f4e3a6c3b87219186b253a0ca634a068
NGuljajev <nguljajev@gmail.com>
2025-10-07 14:06:23 +0300

    add cookie and sessions example

--- routes.php
+++ routes.php
@@ -2,6 +2,7 @@
 
 use App\Controllers\PostsController;
 use App\Controllers\PublicController;
+use App\Controllers\AuthController;
 use App\Router;
 
 Router::get('/', [PublicController::class, 'index']);
@@ -18,3 +19,6 @@ Router::get('/posts/view', [PostsController::class, 'view']);
 Router::get('/posts/edit', [PostsController::class, 'edit']);
 Router::post('/posts/edit', [PostsController::class, 'update']);
 Router::get('/posts/delete', [PostsController::class, 'destroy']);
+
+
+Router::get('/register', [AuthController::class, 'registerForm']);
